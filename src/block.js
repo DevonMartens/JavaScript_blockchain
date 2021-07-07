3186421b1ffdd67b954413eb91ca255d4da074a9
DevonMartens <dmartens@2u.com>
2021-07-07 12:53:01 -0400

    fixes error

--- src/block.js
+++ src/block.js
@@ -68,7 +68,7 @@ class Block {
         // Getting the encoded data saved in the Block
         let encodedData = this.body;
         // Decoding the data to retrieve the JSON representation of the object
-        let JSONdata = this hex2ascii(encodedData);
+        let JSONdata = this.hex2ascii(encodedData);
         // Parse the data to an object to be retrieve.
         let finalData = JSON.parse(JSONdata);
         // Resolve with the data if the object isn't the Genesis block
